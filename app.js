925ef56f0a41068558064aaa5138d59ca9403ecf
Maryam <maryamjaved.xo@gmail.com>
2023-07-10 19:03:32 +0500

    finding single documents

--- app.js
+++ app.js
@@ -1,74 +1,61 @@
-// const express=require('express')
-// const { getDb, connectToDb } = require('./db')
-
-// // init app and middleware
-
-// const app=express()
-
-// // db connection
-// let db
-
-// connectToDb((err) => {   //its going to connect to db for us
-//   if(!err){
-//     app.listen('3000', () => {
-//       console.log('app listening on port 3000')
-//     })
-//     db = getDb()  //going to return db connection object we need
-//   }
-// })
-
-
-// // routes
-// app.get('/books', (req, res)=>{
-//     let books = []
-
-//     db.collection('books')
-//     // find all books
-//       .find()            //find method returns us something as cursor. with use method on cursor we can do smth like foreach or toarray
-//        //it fetch docuents in batches like 100 in each batch then it will fetch another batch    
-//       .sort({author: 1})    //sort alphabetically by author name
-//     //   cursor method
-//       .forEach(book => books.push(book))
-//       .then(() => {
-//         res.status(200).json(books)   //sending books array back to client
-//       })
-//       .catch(() => {
-//         res.status(500).json({error: 'Could not fetch the documents'})
-//       })
-// })
-
-
-
-const express = require('express')
+const express=require('express')
 const { getDb, connectToDb } = require('./db')
+const { ObjectId } = require('mongodb')   //bcs we are using objectID from mongo db
+ 
 
-// init app & middleware
-const app = express()
+// init app and middleware
+
+const app=express()
 
 // db connection
 let db
 
-connectToDb((err) => {
+connectToDb((err) => {   //its going to connect to db for us
   if(!err){
     app.listen('3000', () => {
       console.log('app listening on port 3000')
     })
-    db = getDb()
+    db = getDb()  //going to return db connection object we need
   }
 })
 
-// routes
-app.get('/books', (req, res) => {
-  let books = []
 
-  db.collection('books')
-    .find()
-    .sort({author: 1})
-    .forEach(book => books.push(book))
-    .then(() => {
-      res.status(200).json(books)
-    })
-    .catch(() => {
-      res.status(500).json({error: 'Could not fetch the documents'})
-    })
+// routes
+app.get('/books', (req, res)=>{
+    let books = []
+
+    db.collection('books')
+    // find all books
+      .find()            //find method returns us something as cursor. with use method on cursor we can do smth like foreach or toarray
+       //it fetch docuents in batches like 100 in each batch then it will fetch another batch    
+      .sort({author: 1})    //sort alphabetically by author name
+    //   cursor method
+      .forEach(book => books.push(book))
+      .then(() => {
+        res.status(200).json(books)   //sending books array back to client
+      })
+      .catch(() => {
+        res.status(500).json({error: 'Could not fetch the documents'})
+      })
 })
+
+
+app.get('/books/:id', (req, res) => {
+
+  if (ObjectId.isValid(req.params.id)) {   
+   
+    db.collection('books')
+      .findOne({_id: new ObjectId(req.params.id)})
+      .then(doc => {
+        res.status(200).json(doc)
+      })
+      .catch(err => {
+        res.status(500).json({error: 'Could not fetch the document'})
+      })
+      
+  } else {
+    res.status(500).json({error: 'Could not fetch the document'})
+  }
+
+})
+
--- db.js
+++ db.js
@@ -1,44 +1,26 @@
-// const { MongoClient } = require('mongodb')   //allow us to connect to db
-
-// let dbConnection
-
-// module.exports={
-//     //to initially connect to database    
-//     connectToDb:(cb)=>{     //we are gonna return callback function cb from another file after connecting to db    
-//         // this is connection string for local databse in computer 
-//         MongoClient.connect('mongodb://localhost:27017/bookstore') //arg is connection string(in order to connect to right db)
-//         .then(client => {    //client we just created connected to db
-//             dbConnection = client.db()    //db returns interface through which we can interact with db we're connected to  
-//             return cb()
-//           })
-//           .catch(err => {
-//             console.log(err)
-//             return cb(err)
-//         })
-//     },
-    
-//     getDb: () => dbConnection
-//     //return our db connection
-    
-    
-// }
-
-
-const { MongoClient } = require('mongodb')
+const { MongoClient } = require('mongodb')   //allow us to connect to db
 
 let dbConnection
 
-module.exports = {
-  connectToDb: (cb) => {
-    MongoClient.connect('mongodb://0.0.0.0:27017/bookstore')
-      .then(client => {
-        dbConnection = client.db()
-        return cb()
-      })
-      .catch(err => {
-        console.log(err)
-        return cb(err)
-      })
-  },
-  getDb: () => dbConnection
+module.exports={
+    //to initially connect to database    
+    connectToDb:(cb)=>{     //we are gonna return callback function cb from another file after connecting to db    
+        // this is connection string for local databse in computer 
+        MongoClient.connect('mongodb://0.0.0.0:27017/bookstore') //arg is connection string(in order to connect to right db)
+        .then(client => {    //client we just created connected to db
+            dbConnection = client.db()    //db returns interface through which we can interact with db we're connected to  
+            return cb()
+          })
+          .catch(err => {
+            console.log(err)
+            return cb(err)
+        })
+    },
+    
+    getDb: () => dbConnection
+    //return our db connection
+    
+    
 }
+
+
