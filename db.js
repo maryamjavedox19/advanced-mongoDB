36e8f20a10f2e69f7dcf5fc8f619399a7689da26
Maryam <maryamjaved.xo@gmail.com>
2023-07-11 00:21:54 +0500

    mongoDB atlas

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 const { MongoClient } = require('mongodb')   //allow us to connect to db
 
 let dbConnection
+let uri="mongodb+srv://mariumxyz:<brooklyn99>@cluster0.oawpejp.mongodb.net/?retryWrites=true&w=majority";
 
 module.exports={
     //to initially connect to database    
     connectToDb:(cb)=>{     //we are gonna return callback function cb from another file after connecting to db    
         // this is connection string for local databse in computer 
-        MongoClient.connect('mongodb://0.0.0.0:27017/bookstore') //arg is connection string(in order to connect to right db)
+        MongoClient.connect(uri) //arg is connection string(in order to connect to right db)
         .then(client => {    //client we just created connected to db
             dbConnection = client.db()    //db returns interface through which we can interact with db we're connected to  
             return cb()
