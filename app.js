fd6a2701d5cdbab1bdf4929f14f0591a4b4e7bf6
Maryam <maryamjaved.xo@gmail.com>
2023-07-10 18:28:21 +0500

    cursor and fetching data

--- app.js
+++ app.js
@@ -1,24 +1,74 @@
-const express=require('express')
+// const express=require('express')
+// const { getDb, connectToDb } = require('./db')
+
+// // init app and middleware
+
+// const app=express()
+
+// // db connection
+// let db
+
+// connectToDb((err) => {   //its going to connect to db for us
+//   if(!err){
+//     app.listen('3000', () => {
+//       console.log('app listening on port 3000')
+//     })
+//     db = getDb()  //going to return db connection object we need
+//   }
+// })
+
+
+// // routes
+// app.get('/books', (req, res)=>{
+//     let books = []
+
+//     db.collection('books')
+//     // find all books
+//       .find()            //find method returns us something as cursor. with use method on cursor we can do smth like foreach or toarray
+//        //it fetch docuents in batches like 100 in each batch then it will fetch another batch    
+//       .sort({author: 1})    //sort alphabetically by author name
+//     //   cursor method
+//       .forEach(book => books.push(book))
+//       .then(() => {
+//         res.status(200).json(books)   //sending books array back to client
+//       })
+//       .catch(() => {
+//         res.status(500).json({error: 'Could not fetch the documents'})
+//       })
+// })
+
+
+
+const express = require('express')
 const { getDb, connectToDb } = require('./db')
 
-// init app and middleware
-
-const app=express()
+// init app & middleware
+const app = express()
 
 // db connection
 let db
 
-connectToDb((err) => {   //its going to connect to db for us
+connectToDb((err) => {
   if(!err){
     app.listen('3000', () => {
       console.log('app listening on port 3000')
     })
-    db = getDb()  //going to return db connection object we need
+    db = getDb()
   }
 })
 
-
 // routes
-app.get('/books', (req, res)=>{
-    res.json({msg:"welcome to api"})
+app.get('/books', (req, res) => {
+  let books = []
+
+  db.collection('books')
+    .find()
+    .sort({author: 1})
+    .forEach(book => books.push(book))
+    .then(() => {
+      res.status(200).json(books)
+    })
+    .catch(() => {
+      res.status(500).json({error: 'Could not fetch the documents'})
+    })
 })
